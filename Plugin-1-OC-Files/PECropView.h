7ba4898ab2ae12ee6059afe371297970e2c4db18
Terasol Technologies <32329008+TerasolTechnologies@users.noreply.github.com>
2017-09-27 18:18:54 +0530

    Add files via upload

---
+++ Plugin-1-OC-Files/PECropView.h
@@ -0,0 +1,46 @@
+//
+//  PECropView.h
+//  PhotoCropEditor
+//
+//  Created by kishikawa katsumi on 2013/05/19.
+//  Copyright (c) 2013 kishikawa katsumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface PECropView : UIView
+
+@property (nonatomic) UIImage *image;
+
+@property (nonatomic, readonly) UIImage *croppedImage;
+
+@property (nonatomic, readonly) CGAffineTransform rotation;
+
+@property (nonatomic, readonly) BOOL userHasModifiedCropArea;
+
+@property (nonatomic) BOOL keepingCropAspectRatio;
+
+@property (nonatomic) CGFloat cropAspectRatio;
+
+@property (nonatomic) CGFloat customZoom;
+
+@property (nonatomic) CGRect cropRect;
+
+@property (nonatomic) CGRect imageCropRect;
+
+@property (nonatomic) CGFloat rotationAngle;
+
+@property (nonatomic, readonly) CGRect zoomedCropRect;
+@property (nonatomic, weak, readonly) UIRotationGestureRecognizer *rotationGestureRecognizer;
+
+- (void)resetCropRect;
+
+- (void)resetCropRectAnimated:(BOOL)animated;
+
+- (void)setRotationAngle:(CGFloat)rotationAngle snap:(BOOL)snap;
+
+- (void)setAVar:(int)newAVar;
+
+@end
